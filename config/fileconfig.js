f6ca1053908af5825847cb9dab4be354559f3b1c
yunwisdom <qq695926920@163.com>
2021-06-16 17:59:00 +0800

    init

--- config/fileconfig.js
+++ config/fileconfig.js
@@ -5,9 +5,9 @@
 const config = {
     path: '/data/filesystem',
     imageurl: '/message-background.jpg',
-    manage: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2da6821a-36d3-495b-8092-7220066a0cd8',
-    front: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=181fbd10-47c5-4c61-ba2d-3c4762d5a64f',
-    done: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=07b69342-4b35-4b2e-ad69-249e23c57c21',
+    manage: '',
+    front: '',
+    done: '',
 };
 
 module.exports = {
